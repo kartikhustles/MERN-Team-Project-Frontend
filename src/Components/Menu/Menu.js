db2c7fb3629bf2f74a3b4a4ce7505e9550bce2f8
adibafaiyaz16@gmail.com <adibafaiyaz16@gmail.com>
2023-11-02 17:19:11 +0530

    Contact

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,9 +1,9 @@
 
 function Menu()
 {
-    return <h1 className="text-3xl font-bold underline">
-    Hello wor!
-  </h1>
+    return (
+      <h1>hehe</h1>
+    )
 }
 
 export default Menu;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -8,7 +8,7 @@ function Nav()
             <div className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <Link to='/' className='block py-2 pl-3 pr-4 rounded md:bg-transparent'>Home</Link>
             <Link to='/about' className='block py-2 pl-3 pr-4 rounded md:bg-transparent'>About</Link>
-            <Link to='/menu' className='block py-2 pl-3 pr-4 rounded md:bg-transparent'>Menu</Link>
+            <Link to='/menu' className='block py-2 pl-3 pr-4 rounded md:bg-transparent'>Contact</Link>
             </div>
             </div>
         </nav>
