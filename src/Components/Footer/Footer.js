8a02574dc08b4e8f4726e87e4cbbc75d975e4cb9
adibafaiyaz16@gmail.com <adibafaiyaz16@gmail.com>
2023-11-02 12:51:32 +0530

    Hero-section

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
     return (
 
         <footer class="bg-white w-full shadow  dark:bg-gray-800 flex justify-end">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <div class="w-full mx-auto max-w-screen-xl p-5 md:flex md:items-center md:justify-between">
                 <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
                 </span>
                 <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,7 +3,7 @@ function Nav()
 {
     return(
         <nav className='bg-white border-gray-200 dark:bg-gray-900 text-slate-100'>
-            <div className='flex flex-wrap items-center justify-between mx-auto p-4'>
+            <div className='flex flex-wrap items-center justify-between mx-auto p-2'>
             <Link to='/' className="">Dine</Link>
             <div className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <Link to='/' className='block py-2 pl-3 pr-4 rounded md:bg-transparent'>Home</Link>
