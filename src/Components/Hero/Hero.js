390a370fb78aa23aa3722bcbd386987c795173de
Kartik Agarwal <kartikhustles@gmail.com>
2023-11-03 16:58:20 +0530

    Hero Section Updated

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,42 +1,47 @@
+import "../../index.css";
 
-function Hero()
-{
-    return (
-        <>
-        <div className='w-full bg-new-400 h-auto flex'>
-          <div className='w-2/3'>
-          <img className='h-full' src="https://images.unsplash.com/photo-1521017432531-fbd92d768814?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
-          </div>
-          <div className='w-1/3 flex justify-center'>
-            <div className='w-[80%] h-4/5 bg-new-800 mt-10 border-double border-4 border-sky-500 rounded-3xl justify-center align-center text-slate-100' >
-              <h3 className='text-2xl pt-5 px-10'>Let us Serve you better</h3>
-              <p className='text-md py-3 px-4 text-center'>
-              Don’t wait in a line to enjoy your meal. Reserve a table in advance with us.
-              </p>
-              <p>Date</p>
-              <p>Time</p>
-              <select className='bg-new-800 text-slate-100'>
-              <option >Availibility</option>
-                                    <option>12:00 PM</option>
-                                    <option>12:30 PM</option>
-                                    <option>01:00 PM</option>
-                                    <option>01:30 PM</option>
-                                    <option>02:00 PM</option>
-                                    <option>02:30 PM</option>
-                                    <option>07:00 PM</option>
-                                    <option>07:30 PM</option>
-                                    <option>08:00 PM</option>
-                                    <option>08:30 PM</option>
-                                    <option>09:00 PM</option>
-                                    <option>09:30 PM</option>
-                                    <option>10:00 PM</option>
-              </select>
-              <button>Reserve Table</button>
-            </div>
-          </div>
+function Hero() {
+  return (
+    <>
+      <div className="w-full hero flex relative">
+        <img
+          className=" h-full w-full object-cover z-[-1]"
+          src="https://images.unsplash.com/photo-1543007631-283050bb3e8c?auto=format&fit=crop&q=80&w=3174&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <div className="flex glassmorphism heromid absolute z-1 w-[50%] flex-col rounded-xl  align-center text-slate-100">
+          <h3 className="text-5xl font-bold pt-5 text-center">
+            Let us Serve you better
+          </h3>
+          <p className="text-lg font-semibold py-2 text-center">
+            Don’t wait in a line to enjoy your meal. Reserve a table in advance
+            with us.
+          </p>
+          <p className="text-xl font-semibold px-5 py-2">Date</p>
+          <p className="text-xl font-semibold px-5 py-2">Time</p>
+          <select className="text-xl font-semibold px-3 py-2 bg-transparent text-slate-100">
+            <option>Availibility</option>
+            <option>12:00 PM</option>
+            <option>12:30 PM</option>
+            <option>01:00 PM</option>
+            <option>01:30 PM</option>
+            <option>02:00 PM</option>
+            <option>02:30 PM</option>
+            <option>07:00 PM</option>
+            <option>07:30 PM</option>
+            <option>08:00 PM</option>
+            <option>08:30 PM</option>
+            <option>09:00 PM</option>
+            <option>09:30 PM</option>
+            <option>10:00 PM</option>
+          </select>
+          <button className="text-xl reservebtn rounded-lg font-semibold px-5 py-2 m-2">
+            Reserve Table
+          </button>
         </div>
-        </>
-      )
+      </div>
+    </>
+  );
 }
 
 export default Hero;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,18 +1,34 @@
-import {Link} from 'react-router-dom'
-function Nav()
-{
-    return(
-        <nav className='bg-white border-gray-200 dark:bg-gray-900 text-slate-100'>
-            <div className='flex flex-wrap items-center justify-between mx-auto p-2'>
-            <Link to='/' className="">Dine</Link>
-            <div className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-            <Link to='/' className='block py-2 pl-3 pr-4 rounded md:bg-transparent'>Home</Link>
-            <Link to='/about' className='block py-2 pl-3 pr-4 rounded md:bg-transparent'>About</Link>
-            <Link to='/menu' className='block py-2 pl-3 pr-4 rounded md:bg-transparent'>Contact</Link>
-            </div>
-            </div>
-        </nav>
-    )
+import { Link } from "react-router-dom";
+function Nav() {
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 text-slate-100">
+      <div className="flex flex-wrap items-center justify-between mx-10 p-2">
+        <Link to="/" className=" font-bold text-4xl ">
+          Dine
+        </Link>
+        <div className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <Link
+            to="/"
+            className="block py-2 pl-3 pr-4  font-semibold text-xl  rounded md:bg-transparent"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="block py-2 pl-3 pr-4 font-semibold text-xl rounded md:bg-transparent"
+          >
+            About
+          </Link>
+          <Link
+            to="/menu"
+            className="block py-2 pl-3 pr-4 font-semibold text-xl rounded md:bg-transparent"
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
