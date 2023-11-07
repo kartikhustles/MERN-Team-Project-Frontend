4848292518977326b4c3edbef9435792191e6614
Vaibhav <vaibhavkawatra1133@gmail.com>
2023-11-07 23:35:46 +0530

    Font Changed

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
 
 function Hero() {
+  const customFontStyle = {
+    fontFamily: 'Dancing, sans-serif', 
+    fontSize: '4rem',
+  };
+  const customFontStyle2 = {
+    fontFamily: 'Garet-Book, sans-serif',  
+    fontSize: '1.5rem',
+  };
+
+
   return (
     <>
       <div className="w-full hero flex relative">
@@ -9,19 +19,19 @@ function Hero() {
           src="https://images.unsplash.com/photo-1543007631-283050bb3e8c?auto=format&fit=crop&q=80&w=3174&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
-        <div className="flex glassmorphism heromid absolute z-1 w-[50%] flex-col rounded-xl  align-center text-slate-100">
-          <h3 className="text-5xl font-bold pt-5 text-center">
-            Let us Serve you better
-          </h3>
-          <p className="text-lg font-semibold py-2 text-center">
-            Don’t wait in a line to enjoy your meal. Reserve a table in advance
-            with us.
+        <div className="flex glassmorphism heromid absolute z-1 w-[50%] flex-col rounded-xl align-center text-slate-100">
+        <h3 className="text-5xl font-bold pt-5 text-center" style={{ ...customFontStyle, lineHeight: '1.5' }}>
+  Let us Serve you better
+</h3>
+
+            <p className="text-lg font-semibold py-2 text-center" style={{ ...customFontStyle2, lineHeight: '1.5' }}>
+            Skip the line and reserve a table with us.
           </p>
 
           <button className="mx-auto text-xl reservebtn rounded-lg font-semibold px-5 py-2 m-2">
             <Link
               to="/reserve"
-              className="block py-2 pl-3 pr-4 font-semibold text-xl rounded md:bg-transparent"
+              className="block py-2 pl-3 pr-4 font-semibold text-xl rounded md:bg-transparent" style={{ ...customFontStyle2, lineHeight: '1.5' }}
             >
               Reserve Table
             </Link>
