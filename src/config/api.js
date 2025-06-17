bb65991e6ca351177c7a9c6c3326a83625289bff
AdibaFaiyaz <adibafaiyaz16@gmail.com>
2025-06-17 06:06:50 +0530

    Booking table data Save

--- src/config/api.js
+++ src/config/api.js
@@ -7,7 +7,9 @@ const API_BASE_URL = process.env.REACT_APP_API_URL ||
 export const API_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/people/login`,
   SIGNUP: `${API_BASE_URL}/people/create-people`,
-  BOOKINGS: `${API_BASE_URL}/bookings`
+  BOOKINGS: `${API_BASE_URL}/bookings`,
+  CREATE_BOOKING: `${API_BASE_URL}/bookings/create`,
+  CHECK_AVAILABILITY: (restaurantName, date) => `${API_BASE_URL}/bookings/availability/${encodeURIComponent(restaurantName)}/${date}`
 };
 
 export default API_BASE_URL;
